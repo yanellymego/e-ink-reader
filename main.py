11a7bbcd874adb3e9af64ff5b507b6ae83dcb116
yanellymego <89563053+yanellymego@users.noreply.github.com>
2026-09-24 00:14:13 -0700

    Removed comments

--- main.py
+++ main.py
@@ -4,32 +4,3 @@ import display
 if __name__ == "__main__":
     display.init_display()
     menu.main()
-
-
-
-##as of 9/23
-# for the most part, the book formatting is fine. Needs minor modification
-# settings page implemented and works normally as of first round of manual testing
-# FBReader is still a wip
-
-##to implement
-# some way to save progress of book to continue where I left off
-    # maybe add a "pick off where left" button in the main menu if possible
-
-# add more books to test reading experience
-    # I think there's a way to set this up wirelessly
-
-# implement other reader
-    # make sure it can read regular files
-    # get netgalley protected file and attempt with as well
-
-
-##to test
-# Manga
-# Light novels
-
-
-##finished
-# some way to "double tap" to skip to next section page (basically I want to be able to skip table of content)
-# "pick off where left" already implemented in the book itself, not main menu yet
-# settings page finished
